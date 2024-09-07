703eae535b75ad7d99830b6c2354102c96a1d123
abdelrahman <abdo.aly.k.m@gmail.com>
2024-09-07 17:54:58 +0300

    fix

--- src/app/Interceptors/error.interceptor.ts
+++ src/app/Interceptors/error.interceptor.ts
@@ -1,11 +1,13 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { catchError, throwError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  //const _ToastrService = inject(ToastrService);
+  const _ToastrService = inject(ToastrService);
   return next(req).pipe(
     catchError((err: HttpErrorResponse) => {
-      // _ToastrService.error(err.error.message, "FreshCart");
+      _ToastrService.error(err.error.message, 'Souqmart');
       console.error(err);
       return throwError(() => {
         return err;
