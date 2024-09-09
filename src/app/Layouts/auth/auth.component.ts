c3e09a8be6d85610981003c1a0194fb0da8baff3
abdelrahman <abdo.aly.k.m@gmail.com>
2024-09-09 18:32:06 +0300

    add feat notfound component

--- src/app/Layouts/auth/auth.component.ts
+++ src/app/Layouts/auth/auth.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavBarAuthComponent } from '../../Components/nav-bar-auth/nav-bar-auth.component';
 import { RouterOutlet } from '@angular/router';
+import { LogInComponent } from "../../Components/log-in/log-in.component";
+import { FooterComponent } from "../../Components/footer/footer.component";
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [NavBarAuthComponent, RouterOutlet],
+  imports: [NavBarAuthComponent, RouterOutlet, LogInComponent, FooterComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
--- src/app/Layouts/blank/blank.component.ts
+++ src/app/Layouts/blank/blank.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavBarBlankComponent } from '../../Components/nav-bar-blank/nav-bar-blank.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../../Components/footer/footer.component";
 
 @Component({
   selector: 'app-blank',
   standalone: true,
-  imports: [NavBarBlankComponent,RouterOutlet],
+  imports: [NavBarBlankComponent, RouterOutlet, FooterComponent],
   templateUrl: './blank.component.html',
   styleUrl: './blank.component.css'
 })
