288f7175b8f9999ec40e34f14abc309da3958a0b
abdelrahman <abdo.aly.k.m@gmail.com>
2024-09-09 19:14:04 +0300

    add log in and register forms desgin

--- src/app/Components/log-in/log-in.component.ts
+++ src/app/Components/log-in/log-in.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-log-in',
-  standalone: true,
-  imports: [],
-  templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.css'
+	selector: 'app-log-in',
+	standalone: true,
+	imports: [RouterLink],
+	templateUrl: './log-in.component.html',
+	styleUrl: './log-in.component.css',
 })
-export class LogInComponent {
-
-}
+export class LogInComponent {}
