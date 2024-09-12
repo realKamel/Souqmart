675c520f70917ff93b5b132dd446017172c3020f
abdelrahman <abdo.aly.k.m@gmail.com>
2024-09-12 21:40:22 +0300

    setting toast lib

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,36 +1,36 @@
 import { ApplicationConfig } from '@angular/core';
 import {
-  provideRouter,
-  withInMemoryScrolling,
-  withViewTransitions,
+	provideRouter,
+	withInMemoryScrolling,
+	withViewTransitions,
 } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
+	provideHttpClient,
+	withFetch,
+	withInterceptors,
 } from '@angular/common/http';
 import { errorInterceptor } from './Interceptors/error.interceptor';
 import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(
-      routes,
-      withViewTransitions(),
-      withInMemoryScrolling({ scrollPositionRestoration: 'top' })
-    ),
-    provideClientHydration(),
-    provideAnimations(),
-    provideHttpClient(withFetch(), withInterceptors([errorInterceptor])),
-    provideToastr({
-      newestOnTop: true,
-      preventDuplicates: true,
-      countDuplicates: true,
-      closeButton: true,
-    }),
-  ],
+	providers: [
+		provideRouter(
+			routes,
+			withViewTransitions(),
+			withInMemoryScrolling({ scrollPositionRestoration: 'top' })
+		),
+		provideClientHydration(),
+		provideAnimations(),
+		provideHttpClient(withFetch(), withInterceptors([errorInterceptor])),
+		provideToastr({
+			newestOnTop: true,
+			preventDuplicates: true,
+			countDuplicates: true,
+			resetTimeoutOnDuplicate: true,
+		}),
+	],
 };
