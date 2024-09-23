f41683772a902c920bbf98036faf21aa762c91d0
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-09-23 17:52:42 +0300

    adding ngPrime

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js" ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require( 'flowbite/plugin' )
-  ],
-};
-
+	content: [
+		"./src/**/*.{html,ts}",
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	corePlugins: {
+		preflight: false,
+	}
+}
