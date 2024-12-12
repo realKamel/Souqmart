14aea0dbe632baed2ce9c8483b126e671667eb54
abdelrahman <abdo.aly.k.m@gmail.com>
2024-12-12 19:55:48 +0200

    init files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
+import { TuiRoot } from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Souq';
